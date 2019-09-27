113c078ce4308f439bf6273633c931654b376944
odinokun <odinokun@gmail.com>
2019-09-27 15:37:31 +0300

    did comments from 2019-09-27

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ $('.info-main__item span').on('click', function () {
   var dataVal = $(this).parents('.info-main__item').data('item');
   $('.aside').fadeOut();
   $('#aside-' + dataVal).fadeIn();
+  $('#popup-layer').fadeIn();
 });
 
 $('.aside__close').on('click', function () {
   $('.info-main__item span').removeClass('active');
-  $('.aside').fadeOut();
+  $('.aside, #popup-layer').fadeOut();
 });
 
